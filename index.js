88f05eebf3b74283fe411f3f8a452b257b136fd9
talmobi talmo.christian@gmail.com <talmo.christian@gmail.com>
2016-03-31 02:18:19 +0000

    decoupled twitch and database, added mongodb alongside rethinkdb

--- index.js
+++ index.js
@@ -1,40 +1,21 @@
 var net = require('net');
 
-var auth = require('./private/auth.json');
-
-var messageCounter = 0;
-var savedMessageCounter = 0;
-var started_at = Date.now();
-var channels_active = [];
-
-var connection = null;
-r = require('rethinkdb');
-r.connect({ host: 'localhost', port: 28015 }, function (err, conn) {
-  if (err) throw err;
-  connection = conn;
-
-  started_at = Date.now();
-
-  r.db('test').tableCreate('messages').run(conn, function (err, res) {
-    if (err) {
-      if (err.msg.indexOf('already exists') < 0) {
-        throw err;
-      }
-    } else {
-      console.log(res);
-    }
-  });
+function start (opts, callback) {
+  if (!opts || typeof opts !== 'object' || !opts.host || !opts.port ||
+      !opts.oauth || !opts.nick) {
+    return new Error("opts error. See: { host, port, channels, oauth, nick }" );;
+  }
 
   // connect to twitch irc
-  var client = net.connect({ host: auth.host, port: auth.port });
+  var client = net.connect(opts);
 
   client.on('connect', function () {
-    console.log("connected to %s", auth.host);
+    console.log("connected to %s", opts.host);
 
-    // authenticate with twitch
-    console.log("sending twitch oauth authentication...");
-    client.write("PASS " + auth.oauth + "\n");
-    client.write("NICK " + auth.nick + "\n");
+    // opts.nticate with twitch
+    console.log("sending twitch oauth.authentication...");
+    client.write("PASS " + opts.oauth + "\n");
+    client.write("NICK " + opts.nick + "\n");
   });
 
   var dataCounter = 0;
@@ -47,7 +28,7 @@ r.connect({ host: 'localhost', port: 28015 }, function (err, conn) {
 
     //console.log("received data: %s", str);
 
-    // parse the message
+    // split data into lines of text
     buffer += str;
     while (buffer.indexOf('\n') >= 0) {
       var newLineIndex = buffer.indexOf('\n');
@@ -65,7 +46,7 @@ r.connect({ host: 'localhost', port: 28015 }, function (err, conn) {
       joined = true;
       console.log("joining channels..");
 
-      auth.channels.forEach(function (val, ind, arr) {
+      opts.channels.forEach(function (val, ind, arr) {
         var channel = val[0] == '#' ? val : ('#' + val);
         client.write("JOIN " + channel + "\n");
       });
@@ -74,39 +55,20 @@ r.connect({ host: 'localhost', port: 28015 }, function (err, conn) {
 
   client.on('end', function () {
     // exit (and let pm2/forever restart for a reconnection )
-    console.log("disconnected from %s", auth.host);
-	setTimeout(function () {
-		process.exit(1); // exit failure, let pm2 restart
-	}, 5000);
+    console.log("disconnected from %s", opts.host);
+    callback(new Error("disconnected from server - process exiting soon"), null);
+    setTimeout(function () {
+      stop();
+    }, 1000 * 30);
   });
 
-  /*
-  r.db('test').tableCreate('tv_shows').run(conn, function (err, res) {
-    if (err) throw err;
-    console.log(res);
-
-    var doc = {
-      name: "Star Trek TNG"
-    };
-
-    r.table('tv_shows').insert(doc).run(conn, function (err, res) {
-      if (err) throw err;
-      console.log(res);
-    });
-  });
-  */
-
-});
-
-function handleMessage (client, msg) {
-  switch (msg.command.trim()) {
-    case 'PING':
-        client.write("PONG " + auth.host + "\n");
+  function handleMessage (client, msg) {
+    switch (msg.command.trim()) {
+      case 'PING':
+        client.write("PONG " + opts.host + "\n");
         console.log("PONG!");
-      break;
-    case 'PRIVMSG':
-        messageCounter++;
-
+        break;
+      case 'PRIVMSG':
         var chatMessageIndexOf = msg.params.indexOf(':');
         var channel = msg.params.slice(0, chatMessageIndexOf).trim();
         var chatMessage = msg.params.slice(chatMessageIndexOf + 1).trim();
@@ -119,110 +81,93 @@ function handleMessage (client, msg) {
           message: chatMessage,
           created_at: Date.now()
         };
-        r.table('messages').insert(doc).run(connection, function (err, res) {
-          if (err) {
-            throw err;
-          }
-          savedMessageCounter++;
-          if (channels_active.indexOf(doc.channel) == -1) {
-            channels_active.push(doc.channel);
-          }
-          //console.log("saved message to database successfully from user: %s", user);
-        });
-      break;
+
+        callback(null, doc);
+        break;
+    };
+  };
+
+  function buildIRCMessage (input) {
+    /** RFC 1459, 2.3.1
+     * <message>  ::= [':' <prefix> <SPACE> ] <command> <params> <crlf>
+     * <prefix>   ::= <servername> | <nick> [ '!' <user> ] [ '@' <host> ]
+     * <command>  ::= <letter> { <letter> } | <number> <number> <number>
+     * <SPACE>    ::= ' ' { ' ' }
+     * <params>   ::= <SPACE> [ ':' <trailing> | <middle> <params> ]
+     * <middle>   ::= <Any *non-empty* sequence of octets not including SPACE
+     *                or NUL or CR or LF, the first of which may not be ':'>
+     *                <trailing> ::= <Any, possibly *empty*, sequence of octets not including
+     *                                NUL or CR or LF>
+     *                                <crlf>     ::= CR LF
+     *                                */
+
+    var str = input.trim();
+
+    var prefix = null; // optional
+    var command = null; // required
+    var params = null; // required
+
+    //parse prefix
+    if (str[0] === ':') { // optional <prefix> found
+      var l = prefixString = str.slice(0, str.indexOf(' ')).trim();
+      var indexOfUser = l.indexOf('!');
+      var indexOfHost = l.indexOf('@');
+
+      var host = ''; // optional
+      if (~indexOfHost) {
+        host = l.slice( indexOfHost );
+        l = l.slice(0, -host.length);
+      }
+
+      var user = ''; // optional
+      if (~indexOfUser) {
+        user = l.slice( indexOfUser );
+        l = l.slice(0, -user.length);
+      }
+
+      var name = ''; // either <servername> or <nick>
+      name = l.slice(1);
+
+      prefix = {
+        name: name,
+        user: user,
+        host: host,
+        //data: prefixString
+      }
+
+      str = str.slice(prefixString.length); // cut out the prefix part
+    } // eof prefix parse
+
+    // str is now assumed to be without the prefix information
+    str = str.trim();
+    var indexOfParams = str.indexOf(' ');
+    var command = str.slice(0, indexOfParams).trim();
+    var params = str.slice(indexOfParams).trim();
+
+    // check optionals
+    if (params[0] === ':') { // <trailing> found
+    } else { // <middle> <params> found
+    }
+
+    // return a message object
+    return {
+      raw: input,
+        prefix: prefix,
+        command: command,
+        params: params
+    }
+  };
+
+  function stop () {
+    callback(new Error("disconnected from server - process exiting"), null);
+    client.end();
+  };
+
+  return function () {
+    stop();
   };
 };
 
-var LogTimer = {
-  max: 30000,
-  next: 2000,
-  plus: 1000,
-  update: function () {
-    this.next = Math.min( this.max, this.next + this.plus);
-  }
-};
-
-function logger () {
-  console.log("===============");
-  console.log("started at [%s]", new Date(started_at));
-  console.log("[%s / %s] messages saved", savedMessageCounter, messageCounter);
-  console.log("messages lost: [%s]", messageCounter - savedMessageCounter);
-  console.log("channels active: %s", channels_active.join());
-  console.log("===============");
-
-  channels_active = [];
-  LogTimer.update();
-  setTimeout(logger, LogTimer.next);
-};
-logger();
-
-function buildIRCMessage (input) {
-  /** RFC 1459, 2.3.1
-   * <message>  ::= [':' <prefix> <SPACE> ] <command> <params> <crlf>
-   * <prefix>   ::= <servername> | <nick> [ '!' <user> ] [ '@' <host> ]
-   * <command>  ::= <letter> { <letter> } | <number> <number> <number>
-   * <SPACE>    ::= ' ' { ' ' }
-   * <params>   ::= <SPACE> [ ':' <trailing> | <middle> <params> ]
-   * <middle>   ::= <Any *non-empty* sequence of octets not including SPACE
-   *                or NUL or CR or LF, the first of which may not be ':'>
-   *                <trailing> ::= <Any, possibly *empty*, sequence of octets not including
-   *                                NUL or CR or LF>
-   *                                <crlf>     ::= CR LF
-   *                                */
-
-  var str = input.trim();
-
-  var prefix = null; // optional
-  var command = null; // required
-  var params = null; // required
-
-  //parse prefix
-  if (str[0] === ':') { // optional <prefix> found
-    var l = prefixString = str.slice(0, str.indexOf(' ')).trim();
-    var indexOfUser = l.indexOf('!');
-    var indexOfHost = l.indexOf('@');
-
-    var host = ''; // optional
-    if (~indexOfHost) {
-      host = l.slice( indexOfHost );
-      l = l.slice(0, -host.length);
-    }
-
-    var user = ''; // optional
-    if (~indexOfUser) {
-      user = l.slice( indexOfUser );
-      l = l.slice(0, -user.length);
-    }
-
-    var name = ''; // either <servername> or <nick>
-    name = l.slice(1);
-
-    prefix = {
-      name: name,
-      user: user,
-      host: host,
-        //data: prefixString
-    }
-
-    str = str.slice(prefixString.length); // cut out the prefix part
-  } // eof prefix parse
-
-  // str is now assumed to be without the prefix information
-  str = str.trim();
-  var indexOfParams = str.indexOf(' ');
-  var command = str.slice(0, indexOfParams).trim();
-  var params = str.slice(indexOfParams).trim();
-
-  // check optionals
-  if (params[0] === ':') { // <trailing> found
-  } else { // <middle> <params> found
-  }
-
-  // return a message object
-  return {
-    raw: input,
-    prefix: prefix,
-    command: command,
-    params: params
-  }
+module.exports = {
+  start: start
 };
